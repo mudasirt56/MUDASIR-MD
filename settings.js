27351f7e6792aa7873050262cb6577c1740f9ac8
mudasirt56 <mudasirwwe6@gmail.com>
2025-11-13 16:06:32 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settings = {
+  packname: 'MUDASIR-MD Bot',
+  author: '‎',
+  botName: "MUDASIR-MD  Bot",
+  botOwner: 'MUDASIR Official', // Your name
+  ownerNumber: '923239956476', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "👥public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "> ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ: ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ꜰᴏʀ ᴍᴀɴᴀɢɪɴɢ ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ ᴀɴᴅ ᴀᴜᴛᴏᴍᴀᴛɪɴɢ ᴛᴀꜱᴋꜱ.",
+  caption: "𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 MUDASIR-MD",
+  version: "2.2.6 alpha-8",
+  prefix: ".", // prefix (e.g., ., /, !, *)
+  timezone: "Asia/Karachi", //put your country timeZone....leave blank if u don't know
+  updateZipUrl: "https://api.github.com/Mudasirt56/MUDASIR-MD/main/zip",
+};
+
+module.exports = settings;
